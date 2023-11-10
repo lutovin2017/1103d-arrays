1f8a957b47bdaea8c52a1210ededa3edc8299739
Lutovin Sergey <Lutovin2017@gmail.com>
2023-11-10 12:28:18 +0300

    я сделяль)

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 // задача №1
 const fill = (arraySize, value) => {
-  // решение здесь
+const newArr = [];
+ for (let i = 0; i < arraySize; i++) {
+    newArr.push(value);
+  } 
+  return newArr;
 }
 
-const data = 3;
+const data1 = 3;
 const valueToFill = 'a';
-console.log(fill(data, valueToFill))
+console.log(fill(data1, valueToFill))
 
 // задача №2
 const reverse = (array) => {
-  // решение здесь
- }
+  let newArray = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}; // решение здесь
+ 
 
-const data = [1, 2, 3];
-console.log(reverse(data)); // [3, 2, 1]
+const data2 = [1, 2, 3];
+console.log(reverse(data2)); // [3, 2, 1]
 
 // задача №3
-const compact = (array) => {
-  // решение здесь
-}
+const compact = (array)  => {
+   return array.filter((item) => item);
+ }; // решение здесь
 
-const data = [0, 1, false, 2, undefined, '', 3, null];
-console.log(compact(data)) // [1, 2, 3]
+
+const data3 = [0, 1, false, 2, undefined, '', 3, null];
+console.log(compact(data3)) // [1, 2, 3]
